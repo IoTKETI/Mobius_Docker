b746b963689a9ff988a8a14c5a12fd1a4ca14067
Cftn <kman3212@gmail.com>
2020-01-28 17:46:57 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-sudo apt-get install docker.io
+sudo apt-get install -y docker.io
+
+sudo apt-get install -y curl
+
+sudo apt-get install -y npm
 
 sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 
 sudo chmod +x /usr/local/bin/docker-compose
 
-git clone https://github.com/IoTKETI/Mobius_Docker.git
-
-cd Mobius_Docker
-
 git clone https://github.com/IoTKETI/Mobius.git
 
 cd Mobius
 
-npm install
+sudo npm install
 
 cd ..
 
