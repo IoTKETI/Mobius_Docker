715376437a8e6ba2c815f87be75c88f1c3def250
Cftn <kman3212@gmail.com>
2020-01-28 18:02:08 +0900

    Updated install.sh

--- install.sh
+++ install.sh
@@ -18,5 +18,10 @@ sudo npm install
 
 cd ..
 
+sudo docker-compose up --no-start
+
+echo -e "\n===========Result of Mobius docker install==============\n"
+sudo docker-compose ps
+echo -e "\n========================================================\n"
 
 
